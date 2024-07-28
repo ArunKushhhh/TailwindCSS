83fd31c98260076f56d5f20cbe3cfd00bb31b50f
ArunKushhhh <arunkushwaha_230604@gmail.edu.in>
2024-07-29 00:01:36 +0530

    Movie-web-app project concluded

---
+++ 04.Movie-Web-App/tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "index.html",
+    "./src/**/*.{css,html,js,jsx,ts,tsx}", // Adjust the paths according to your project structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
